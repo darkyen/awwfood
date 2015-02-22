8b62a223f65efc4f7acd91a1feae4a2ca7ede342
Abhishek Hingnikar <abhishek@goph.me>
2015-02-22 14:36:19 +0530

    First commit

---
+++ gulp/config.js
@@ -0,0 +1,38 @@
+var dest = "./dist";
+var src = './src';
+
+module.exports = {
+  server: {
+    settings: {
+      root: dest,
+      host: 'localhost',
+      port: 8080,
+      livereload: {
+        port: 35929
+      }
+    }
+  },
+  less: {
+    src: src + "/styles/main.less",
+    dest: dest + "/styles",
+    settings: {
+      paths: ['.', './node_modules/', './public/js/bower_components']
+    }
+  },
+  browserify: {
+    settings: {
+      transform: ['reactify', '6to5ify']
+    },
+    src: src + '/js/index.jsx',
+    dest: dest + '/js',
+    outputName: 'index.js',
+  },
+  html: {
+    src: 'src/index.html',
+    dest: dest
+  },
+  watch: {
+    src: 'src/**/*.*',
+    tasks: ['build']
+  }
+};
---
+++ src/js/constants/AppConstants.js
@@ -0,0 +1,18 @@
+var keyMirror = require('react/lib/keyMirror');
+
+module.exports = {
+
+  CHANGE_EVENT: 'change',
+
+  ActionTypes: keyMirror({
+    ADD_TO_CART: null,
+    REMOVE_FROM_CART: null,
+    CHECKOUT: null
+  }),
+
+  ActionSources: keyMirror({
+    SERVER_ACTION: null,
+    VIEW_ACTION: null
+  })
+
+};
